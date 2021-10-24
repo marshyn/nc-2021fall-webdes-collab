746b0de6607875c1444e83756c0a2854a924f2d8
marshall <70968501+marshyn@users.noreply.github.com>
2021-10-24 17:43:04 -0400

    Add files via upload

---
+++ metz-project/js/dining.js
@@ -0,0 +1,32 @@
+
+
+// navgiation bar
+function Navigation(text, link)
+{
+	this.text = text;
+	this.link = link;
+
+}
+
+
+// Dining Locations and Hours
+
+
+// template object for dining pages
+function Dining(name, location, hours, description)
+{
+	this.name = name;
+	this.location = location;
+	this.hours = hours;
+	this.description = description;
+}
+
+
+
+// HAM object
+var elHAM = document.getElementById('HAM');
+
+
+// Boar's Head object
+
+// Food Pantry object
